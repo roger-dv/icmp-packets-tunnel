3ac56fb120ed987226e320b888d49c29bce7595d
Roger Voss <Roger.Voss@tideworks.com>
2023-04-13 12:39:00 -0700

    scripts now check to see that is being executed by root user (i.e., via sudo) and expects the network namespace to be passed in as the first command line argument (scripts now check and verify that there's a first argument being passed in)

--- scripts/create-att-tst-netns.sh
+++ scripts/create-att-tst-netns.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+if [ "${EUID}" != "0" ]; then
+  echo "ERROR: must be root user"
+  exit 1
+fi
+
+if [ -z "${1}" ]; then
+  echo "ERROR: must supply network namespace name as command line argument"
+  exit 1
+fi
+
+NETNS_NAME="${1}"
+
 # create a namespace
-sudo ip netns add att-tst
+ip netns add "${NETNS_NAME}"
 ip netns list
 
 # bring up the loopback interface on the new network namespace
-sudo ip netns exec att-tst ip link set dev lo up
-sudo ip netns exec att-tst sysctl -w net.ipv4.ping_group_range="0 2147483647"
-sudo ip netns exec att-tst ip address
-
+ip netns exec "${NETNS_NAME}" ip link set dev lo up
+ip netns exec "${NETNS_NAME}" sysctl -w net.ipv4.ping_group_range="0 2147483647"
+ip netns exec "${NETNS_NAME}" ip address
--- scripts/delete-att-tst-netns.sh
+++ scripts/delete-att-tst-netns.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+if [ "${EUID}" != "0" ]; then
+  echo "ERROR: must be root user"
+  exit 1
+fi
+
+if [ -z "${1}" ]; then
+  echo "ERROR: must supply network namespace name as command line argument"
+  exit 1
+fi
+
+NETNS_NAME="${1}"
+
 # delete the network namespace
-sudo ip netns delete att-tst
+ip netns delete "${NETNS_NAME}" 
 ip netns list
